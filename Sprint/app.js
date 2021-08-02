0539ef0ead408a6a2ac984d4d6d14f270684a0d7
MauroAmpuero <mau999ampu@gmail.com>
2021-08-02 16:51:22 -0300

    cambios realizados

--- Sprint/app.js
+++ Sprint/app.js
@@ -1,24 +1,16 @@
-const express= require('express')
-const app = express()
-const path = require('path')
+const express= require('express');
+const app = express();
+const path = require('path');
 
-app.get('/', function(req,res){res.send('Hola Mundo!')})
-app.get('/home', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/index.html'))})
-app.get('/detalleproducto', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/detalleproduc.html'))})
-app.get('/carrito', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/carrito.html'))})
-app.get('/login', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/login.html'))})
-app.get('/registro', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/registro.html'))})
-
-app.get('*', function(req,res){
-    res.send('NOT FOUND')
-})
-
-app.use(express.static('public'))
-
+app.get('/producto', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/producto.html'))});
+app.get('/carrito', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/carrito.html'))});
+app.get('/login', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/login.html'))});
+app.get('/registro', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/registro.html'))});
+app.get('/home', function(reque,resp){resp.sendFile(path.join(__dirname, 'views/index.html'))});
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
 app.listen(3030, ()=> {
     console.log('Pagina corriendo en http://localhost:3030')
-
 })
-
 
 
