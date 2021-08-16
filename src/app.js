e4e9e2bd247bec5d8539554b9b1e813a64faaa3e
Mateo Peralta <map12082004@gmail.com>
2021-08-16 16:43:55 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ const logger = require('morgan');
 const homeRouter = require('./routes/home');
 const usersRouter = require('./routes/users');
 
-
+/*const adminAddRouter = require('');
+const adminRouter = require('');
+*/
 
 app.get('/carrito', (req, res) => {
     res.render('carrito')
