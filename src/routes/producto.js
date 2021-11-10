c8c508bb282d4a812911bc1f3a6cba7d1bf1c3d5
imBerzox <davidmolinadiaz10@gmail.com>
2021-11-09 21:27:52 -0300

    habremos termiado la db?

--- src/routes/producto.js
+++ src/routes/producto.js
@@ -4,15 +4,15 @@ const productoController= require ("../controllers/productoController.js");
 const guestMiddleware = require("../middlewares/guestMiddleware.js");
 const authMiddleware = require("../middlewares/authMiddleware") 
 
-router.get("/lista",productoController.list)
+router.get ("/lista",productoController.list)
 router.get ("/producto",productoController.entrarproducto);
 router.get ("/create",authMiddleware, productoController.create);
 router.post ("/create", productoController.storage);
-router.get('/:id/', productoController.detail);
-router.get('/',productoController.producto1)
-router.get('/:id/edit/',authMiddleware, productoController.edit);
-router.put('/:id/',productoController.upload);
-router.delete('/:id/delete/', productoController.destroy);
+router.get ('/:id/', productoController.detail);
+router.get ('/',productoController.producto1)
+router.get ('/:id/edit/',authMiddleware, productoController.edit);
+router.put ('/:id/',productoController.upload);
+router.delete ('/:id/delete/', productoController.destroy);
 
 
 module.exports = router;
