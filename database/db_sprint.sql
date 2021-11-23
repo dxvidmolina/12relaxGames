f46c214ff548642379990e60cb7db322b4860374
MauroAmpuero <mau999ampu@gmail.com>
2021-11-23 12:36:21 -0300

    cambios

--- database/db_sprint.sql
+++ database/db_sprint.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `db_sprint` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `db_sprint`;
 -- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
 --
 -- Host: localhost    Database: db_sprint
@@ -88,7 +86,7 @@ CREATE TABLE `productos` (
   KEY `productos_FK_1` (`imgref_id`),
   CONSTRAINT `productos_FK` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`),
   CONSTRAINT `productos_FK_1` FOREIGN KEY (`imgref_id`) REFERENCES `imgrefs` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -97,7 +95,7 @@ CREATE TABLE `productos` (
 
 LOCK TABLES `productos` WRITE;
 /*!40000 ALTER TABLE `productos` DISABLE KEYS */;
-INSERT INTO `productos` VALUES (1,'Tribes of Midgard',333,'Un juego increible para jugar',30,1,1);
+INSERT INTO `productos` VALUES (1,'Tribes of Midgard',333,'Un juego increible para jugar',30,1,1),(2,'Battlefield',900,'Battlefield™ 2042 es un juego de disparos en primera persona que marca el regreso de la icónica guerra total de la franquicia. Adáptate en un mundo del futuro cercano transformado por el desorden y vence en campos de batalla cambiantes con la ayuda de tu escuadrón y un arsenal innovado',70,1,1),(3,'Grand Theft Auto V',700,'¡Grand Theft Auto V para PC ofrece a los jugadores la opción de explorar el galardonado mundo de Los Santos y el condado de Blaine con una resolución de 4K y disfrutar del juego a 60 fotogramas por segundo.',15,1,1),(4,'New World',999,'Explora un electrizante videojuego MMO de mundo abierto. Forja tu destino en Aeternum, una isla sobrenatural donde hallarás toda clase de peligros y oportunidades.',10,1,1),(5,'The Ascent',900,'The Ascent es un shooter RPG de acción individual y cooperativo ambientado en un mundo ciberpunk. El Grupo Ascensión, la megacorporación a la que todos pertenecen, ha quebrado. ¿Sobrevivirás sin él?',10,1,1),(6,'Chivalry: MW',2000,'Asedia castillos y asalta pueblos en Chivalry: Medieval Warfare, un trepidante slasher medieval en primera persona que se centra en las batallas multijugador.',30,1,1),(7,'Mass Effect',3000,'Mass Effect™ Legendary Edition incluye el contenido básico para un jugador y más de cuarenta artículos descargables de Mass Effect, Mass Effect 2 y Mass Effect 3, además de armas, armaduras y paquetes promocionales remasterizados y optimizados en 4K Ultra HD.',30,1,1),(8,'Days Gone',1500,'Conduce y lucha en Estados Unidos, un lugar letal tras una pandemia.Como Deacon St. John, un motociclista errante y cazarrecompensas que viaja por el camino averiado y lucha por sobrevivir a la vez que busca una razón para vivir en este juego de acción y aventura en un mundo abierto',15,1,1);
 /*!40000 ALTER TABLE `productos` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -141,4 +139,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-11-23 10:58:30
+-- Dump completed on 2021-11-23 12:33:48
